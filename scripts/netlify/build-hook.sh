9791089f08e8e444a370138650c820779809e3ed
panupongdeve <5635512110@psu.ac.th>
2019-11-28 17:51:25 +0700

    test

--- scripts/netlify/build-hook.sh
+++ scripts/netlify/build-hook.sh
@@ -1,1 +1,2 @@
-curl -X POST -d {} https://api.netlify.com/build_hooks/$BUILD_HOOK_NAME
+# curl -X POST -d {} https://api.netlify.com/build_hooks/$BUILD_HOOK_NAME
+curl -X POST -d {} https://api.netlify.com/build_hooks/5ddfa344c2685aaea015f25d
