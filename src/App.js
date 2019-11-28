69d828e6ed2941cc7dc91d260b3344b90bc0b8a7
Panupong <5635512110@psu.ac.th>
2019-11-29 03:41:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Update with Deploy Azure Successed
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
