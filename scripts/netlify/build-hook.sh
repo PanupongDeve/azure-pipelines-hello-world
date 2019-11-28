fdcf17daf626a6f513505adbdfb363076de3a07f
panupongdeve <5635512110@psu.ac.th>
2019-11-28 18:01:09 +0700

    update

--- scripts/netlify/build-hook.sh
+++ scripts/netlify/build-hook.sh
@@ -1,2 +1,1 @@
-# curl -X POST -d {} https://api.netlify.com/build_hooks/$BUILD_HOOK_NAME
-curl -X POST -d {} https://api.netlify.com/build_hooks/5ddfa344c2685aaea015f25d
+curl -X POST -d {} https://api.netlify.com/build_hooks/$BUILD_HOOK_NAME
